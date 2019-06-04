50bbfd176910b91c8db7fbaedeca8551bdf03b32
Charles McClelland <chip@mcclellands.org>
2019-06-04 18:12:29 -0400

    Added comments to the source file

--- src/TempLogger.ino
+++ src/TempLogger.ino
@@ -10,7 +10,9 @@
 
 // Initialize sensor object.
 DS18 sensor(D4);
-char data[100];
+char data[100];  // This is a big array - yet it is not used.
+// You could define a smaller array here for your Temperature variable
+char temperatureString[16];
 
 
 
@@ -28,11 +30,21 @@ void loop() {
 // Reading data from the sensor.    
    if (sensor.read()) {
      //Sending the temperature in Celsius
-    Particle.publish("temperature", String(sensor.celsius()), PRIVATE);
+    Particle.publish("temperature", String(sensor.celsius()), PRIVATE); 
+    
     //Sending the temperature in  fahrenheit.
     Particle.publish("farenhiet",String(sensor.fahrenheit()),PRIVATE);
 
-    delay(5000);
+    // What if you constructed a more descriptive string for your Particle.variable here
+    // Use snprintf and the temperatureString to construct a variable like: "50 degrees C"
+
+    delay(5000);  // This is a blocking delay
+    /* 
+    You should avoid delays that block program execution.  
+    Consider another approach taking advantage of Particle's waitUntil() function
+    Particle services process requests each time it completes the main loop - your program should not block 
+    the program flow through the main loop.  This gets more important as the code size grows.
+    */
   }
 
 }
