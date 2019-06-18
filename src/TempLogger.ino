337276b2c5860f8ca6c12eaed1573fb7e3b2a99a
Charles McClelland <chip@mcclellands.org>
2019-06-18 17:05:21 -0400

    v1.10 - Fixed reporting for temp change

--- src/TempLogger.ino
+++ src/TempLogger.ino
@@ -28,22 +28,19 @@
 // v1.07 - Made changes for IDLE and VerboseMode. 
 // v1.08 - Fixed Verbose Mode, Cleaned Comments and Added State Transition Monitoring . 
 // v1.09 - Added UBIDots handler and Setup WebHooks.
+// v1.10 - Updated comment formatting and fixed the Ubidots reporting logic for temp change
 
 
-const char releaseNumber[6] = "1.08"; // Displays the release on the menu 
+const char releaseNumber[6] = "1.10";                       // Displays the release on the menu 
 
-#include "DS18.h"             // Include the OneWire library
-
-
-
+#include "DS18.h"                                           // Include the OneWire library
 
 // Initialize modules here
-
-DS18 sensor(D3); // Initialize sensor object
+DS18 sensor(D3);                                            // Initialize the temperature sensor object
 
 
 // State Machine Variables
-enum State
+enum State                                              
 {
   INITIALIZATION_STATE,
   IDLE_STATE,
@@ -51,54 +48,42 @@ enum State
   REPORTING_STATE,
   RESPONSE_WAIT,
   ERROR_STATE
-
-};
-State state = INITIALIZATION_STATE;
+};                                                          // These are the allowed states in the main loop
+State state = INITIALIZATION_STATE;                         // Initialize the state variable
 
 // Variables Related To Particle Mobile Application Reporting
-
-char signalString[16];      // Used to communicate Wireless RSSI and Description
-char temperatureString[16]; // Temperature string for Reporting
-char batteryString[16];     // Battery value for reporting
+char signalString[16];                                      // Used to communicate Wireless RSSI and Description
+char temperatureString[16];                                 // Temperature string for Reporting
+char batteryString[16];                                     // Battery value for reporting
 
 // Variables Related To Update and Refresh Rates. 
+unsigned long updateRate = 5000;                            // Define Update Rate
+static unsigned long refreshRate = 1;                       // Time period for IDLE state. 
 
-unsigned long updateRate = 5000; // Define Update Rate
-static unsigned long refreshRate = 1; // Time period for IDLE state. 
-
-
-bool SetVerboseMode(String command); // Function to Set verbose mode.     *** This is not needed with Particle
-bool verboseMode=true; // Variable VerboseMode. 
-
-float temperatureInC=0; // Current Temp Reading. 
-static float lastTemperatureInC=0;
-
-
-// Setup Particle Variables and Functions here. 
+// Variables releated to the sensors 
+// bool SetVerboseMode(String command);                     // Function to Set verbose mode.     *** This is not needed with Particle
+bool verboseMode=true;                                      // Variable VerboseMode. 
+float temperatureInC=0;                                     // Current Temp Reading global variable
+float voltage;                                              // Voltage level of the LiPo battery - 3.6-4.2V range
 
 void setup()
 {
   getTemperature();
-  Particle.variable("celsius", temperatureString); // Setup Particle Variable
-  Particle.variable("Release", releaseNumber);
-  Particle.variable("Signal", signalString); // Particle variables that enable monitoring using the mobile app
-  Particle.variable("Battery", batteryString);
-  Particle.function("verboseMode", SetVerboseMode);  // Added Particle Function For VerboseMode. 
+  Particle.variable("celsius", temperatureString);          // Setup Particle Variable
+  Particle.variable("Release", releaseNumber);              // So we can see what release is running from the console
+  Particle.variable("Signal", signalString);                // Particle variables that enable monitoring using the mobile app
+  Particle.variable("Battery", batteryString);              // Battery level in V as the Argon does not have a fuel cell
+  Particle.function("verboseMode", SetVerboseMode);         // Added Particle Function For VerboseMode. 
   
   if (verboseMode) Particle.publish("State","IDLE", PRIVATE);
-  state = IDLE_STATE;
-  
-  
+  state = IDLE_STATE;                                       // If we made it this far, we are ready to go to IDLE in the main loop
 }
 
 void loop()
 {
-  
- 
-  switch (state)
+  switch (state)                                            // In the main loop, all code execution must take place in a defined state
   {
   case IDLE_STATE: // IDLE State.
-    
     static unsigned long TimePassed = 0;        
     if (Time.minute() - TimePassed >= refreshRate ) 
     {
@@ -111,42 +96,39 @@ void loop()
     }
   break;
 
-  case MEASURING_STATE: // Measuring State. 
-  
-    getMeasurements(); // Get Measurements and Move to Reporting State. 
-    state = REPORTING_STATE;
-     if(verboseMode){
-      waitUntil(PublishDelayFunction);
-      Particle.publish("State","REPORTING",PRIVATE);
-      
-    } 
+  case MEASURING_STATE:                                    // Measuring State. 
+    if (getMeasurements()) {                               // Get Measurements and Move to Reporting State if there is a change
+      state = REPORTING_STATE;
+      if(verboseMode){
+        waitUntil(PublishDelayFunction);
+        Particle.publish("State","Change detected - Reporting",PRIVATE);
+      } 
+    }
+    else {
+      state = IDLE_STATE;
+      if(verboseMode){
+        waitUntil(PublishDelayFunction);
+        Particle.publish("State","No change - Idle",PRIVATE);
+      } 
+    }
     break;
 
   case REPORTING_STATE: //
     if (verboseMode) Particle.publish("Temperature", temperatureString, PRIVATE); 
-    
-    if(abs(lastTemperatureInC-temperatureInC) >= 1) sendUBIDots();
-   if(verboseMode){
+    sendUBIDots();
+    if(verboseMode){
       waitUntil(PublishDelayFunction);
       Particle.publish("State","IDLE",PRIVATE);
-      
     } 
     state = IDLE_STATE;
-    
     break;
 
-  case RESPONSE_WAIT: // This checks for the response from UBIDOTS. 
-   break;
+  case RESPONSE_WAIT:                                       // This checks for the response from UBIDOTS. 
+  break;
 
-  case ERROR_STATE: // This state RESETS the devices. 
-    break;
-
-
-
+  case ERROR_STATE:                                         // This state RESETS the devices. 
+  break;
   }
-
- 
- 
 }
 
 // Function to create a delay in the publish time
@@ -163,53 +145,47 @@ bool PublishDelayFunction()
 }
 
 // Functions for mobile app reporting. 
-
 void getSignalStrength()
 {
   WiFiSignal sig = WiFi.RSSI();
-
   float rssi = sig.getStrength();
-
   snprintf(signalString, sizeof(signalString), "%.0f%%", rssi);
 }
 
 void getBatteryCharge()
 {
-  float voltage = analogRead(BATT) * 0.0011224;
-
+  voltage = analogRead(BATT) * 0.0011224;
   snprintf(batteryString, sizeof(batteryString), "%3.1f V", voltage);
 }
 
-// Function to get temperature value from DS18B20. 
-    bool getTemperature()
+
+bool getMeasurements()
 {
-  lastTemperatureInC = temperatureInC; 
-  
+  getSignalStrength();                                      // Get Signal Strength
+  getBatteryCharge();                                       // Get Battery Charge Percentage
+  if (getTemperature()) return 1;                           // Read Temperature from Sensor
+  else return 0;                                            // Less than 1 degree difference detected
+}
+
+// Function to get temperature value from DS18B20. 
+bool getTemperature()
+{
+  static float lastTemperatureInC = 0; 
   if (sensor.read())
   { 
     temperatureInC = sensor.celsius();
     snprintf(temperatureString, sizeof(temperatureString), "%3.1f Degrees C", temperatureInC); 
   }
-  return 1;
-}
-
-void getMeasurements()
-{
-
-  getSignalStrength(); // Get Signal Strength
-
-  getBatteryCharge(); // Get Battery Charge Percentage
-
-  getTemperature(); // Read Temperature from Sensor.
-  
-  
+  if (abs(temperatureInC - lastTemperatureInC) >= 1) {
+    lastTemperatureInC = temperatureInC;
+    return 1;
+  }
+  else return 0;
 }
 
 // Function to Toggle VerboseMode. 
-
 bool SetVerboseMode(String command)
 {
-
   if(command == "1")
   {
     verboseMode = true;
@@ -227,13 +203,9 @@ bool SetVerboseMode(String command)
       return 0;
     }
 }
-void sendUBIDots(){
- 
-  if(sensor.read()){
-     char data[256];
-    snprintf(data,sizeof(data),"{\"Temperature\":%3.1f, \"Battery\":%3.1f}",temperatureInC, batteryString);
-    Particle.publish("Air-Quality-Hook",data,PRIVATE);
-    
-    }
-   
-    }
+void sendUBIDots()
+{
+  char data[256];
+  snprintf(data,sizeof(data),"{\"Temperature\":%3.1f, \"Battery\":%3.1f}",temperatureInC, voltage);
+  Particle.publish("Air-Quality-Hook",data,PRIVATE);
+}
